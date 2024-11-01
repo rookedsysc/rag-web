04295361753422f97423f9529ecdb71ea8a66da5
rookedsysc <rookedsysc36@gmail.com>
2024-11-01 23:33:58 +0900

    feat: Add Swagger configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule);
+
+    const config = new DocumentBuilder()
+        .setTitle("RAG Backend")
+        .addTag("rookedsysc")
+        .build();
+    const documents = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("swagger-ui/index.html", app, documents);
+
+    await app.listen(29736);
 }
+
 bootstrap();
